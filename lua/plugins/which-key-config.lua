e67658feb5735df6602db93e796ef598e2c2393d
Vipul Sharma <vipul.sharma20@gmail.com>
2022-01-15 02:41:01 +0530

    remove: unused whichkey config

--- lua/plugins/which-key-config.lua
+++ lua/plugins/which-key-config.lua
@@ -47,108 +47,21 @@ local opts = {
 vim.api.nvim_set_keymap('n', '<Leader>h', ':let @/=""<CR>',
                         {noremap = true, silent = true})
 
--- explorer
-
--- TODO this introduces some bugs unfortunately
--- vim.api.nvim_set_keymap('n', '<Leader>e',
---                         ":NvimTreeToggle<CR>",
---                         {noremap = true, silent = true})
-
--- telescope
-vim.api.nvim_set_keymap('n', '<Leader>e', ':Telescope find_files<CR>',
-                        {noremap = true, silent = true})
-
--- dashboard
-vim.api.nvim_set_keymap('n', '<Leader>;', ':Dashboard<CR>',
-                        {noremap = true, silent = true})
-
--- Comments
-vim.api.nvim_set_keymap("n", "<leader>/", ":CommentToggle<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("v", "<leader>/", ":CommentToggle<CR>",
-                        {noremap = true, silent = true})
-
--- close buffer
-vim.api.nvim_set_keymap("n", "<leader>c", ":BufferClose<CR>",
-                        {noremap = true, silent = true})
-
 -- TODO create entire treesitter section
 
 local mappings = {
 
     ["/"] = "Comment",
     ["c"] = "Close Buffer",
-    ["e"] = "Explorer",
     ["f"] = "Find File",
     ["h"] = "No Highlight",
+    e = {
+        "<cmd>Telescope find_files<cr>", "Find File",
+    },
     b = {
         name = "Buffers",
-        j = {"<cmd>BufferPick<cr>", "jump to buffer"},
         b = {"<cmd>Telescope buffers<cr>", "Find buffer"},
-        w = {"<cmd>BufferWipeout<cr>", "wipeout buffer"},
-        e = {
-            "<cmd>BufferCloseAllButCurrent<cr>", "close all but current buffer"
-        },
-        h = {"<cmd>BufferCloseBuffersLeft<cr>", "close all buffers to the left"},
-        l = {
-            "<cmd>BufferCloseBuffersRight<cr>",
-            "close all BufferLines to the right"
-        },
-        D = {
-            "<cmd>BufferOrderByDirectory<cr>",
-            "sort BufferLines automatically by directory"
-        },
-        L = {
-            "<cmd>BufferOrderByLanguage<cr>",
-            "sort BufferLines automatically by language"
-        }
     },
-
--- diagnostics vanilla nvim
--- -- diagnostic
--- function lv_utils.get_all()
---     vim.lsp.diagnostic.get_all()
--- end
--- function lv_utils.get_next()
---     vim.lsp.diagnostic.get_next()
--- end
--- function lv_utils.get_prev()
---     vim.lsp.diagnostic.get_prev()
--- end
--- function lv_utils.goto_next()
---     vim.lsp.diagnostic.goto_next()
--- end
--- function lv_utils.goto_prev()
---     vim.lsp.diagnostic.goto_prev()
--- end
--- function lv_utils.show_line_diagnostics()
---     vim.lsp.diagnostic.show_line_diagnostics()
--- end
-
-    -- " Available Debug Adapters:
-    -- "   https://microsoft.github.io/debug-adapter-protocol/implementors/adapters/
-    -- " Adapter configuration and installation instructions:
-    -- "   https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
-    -- " Debug Adapter protocol:
-    -- "   https://microsoft.github.io/debug-adapter-protocol/
-    -- " Debugging
-    -- command! DebugToggleBreakpoint lua require'dap'.toggle_breakpoint()
-    -- command! DebugStart lua require'dap'.continue()
-    -- command! DebugContinue lua require'dap'.continue()
-    -- command! DebugStepOver lua require'dap'.step_over()
-    -- command! DebugStepOut lua require'dap'.step_out()
-    -- command! DebugStepInto lua require'dap'.step_into()
-    -- command! DebugToggleRepl lua require'dap'.repl.toggle()
-    -- command! DebugGetSession lua require'dap'.session()
-    -- D = {
-    --     name = "Debug",
-    --     b = {"<cmd>DebugToggleBreakpoint<cr>", "Toggle Breakpoint"},
-    --     c = {"<cmd>DebugContinue<cr>", "Continue"},
-    --     i = {"<cmd>DebugStepInto<cr>", "Step Into"},
-    --     o = {"<cmd>DebugStepOver<cr>", "Step Over"},
-    --     r = {"<cmd>DebugToggleRepl<cr>", "Toggle Repl"},
-    --     s = {"<cmd>DebugStart<cr>", "Start"}
-    -- },
     g = {
         name = "Git",
         j = {"<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk"},
@@ -223,7 +136,6 @@ local mappings = {
         --     "<cmd>Telescope lsp_workspace_diagnostics<cr>",
         --     "Workspace Diagnostics"
         -- },
-        f = {"<cmd>Telescope find_files<cr>", "Find File"},
         h = {"<cmd>Telescope help_tags<cr>", "Find Help"},
         -- m = {"<cmd>Telescope marks<cr>", "Marks"},
         M = {"<cmd>Telescope man_pages<cr>", "Man Pages"},
