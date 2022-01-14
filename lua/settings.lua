f47cdcb86b95af248458ba1379aee116aadc0a74
Vipul Sharma <vipul.sharma20@gmail.com>
2022-01-14 20:25:05 +0530

    fix: general settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,34 +1,19 @@
+-- Tabs and spaces
 vim.o.ts = 4 -- Insert 4 spaces for a tab
 vim.o.sw = 4 -- Change the number of space characters inserted for indentation
 vim.o.expandtab = true  -- Converts tabs to spaces
 
-vim.o.pumheight = 25 -- Makes popup menu smaller
-vim.o.fileencoding = "utf-8" -- The encoding written to file
-vim.o.cmdheight = 1 -- Use 2 for more space for displaying messages
-vim.o.mouse = "a" -- Enable your mouse
-vim.o.timeoutlen = 100 -- By default timeoutlen is 1000 ms
-vim.o.undofile = true -- Enable persistent undo
-vim.opt.list = true
-vim.o.swapfile = false -- Do not write any swp files
-
 -- Colorscheme
 vim.cmd('colorscheme kanagawa')
-vim.cmd('set bg=dark')
-vim.cmd('syntax on') -- syntax highlighting
+vim.o.bg = "dark"
+vim.o.syntax = "on"
 
 -- Text folding stuff
-vim.o.foldmethod = 'indent'
+vim.o.foldmethod = "indent"
 vim.o.foldnestmax = 10
 vim.o.foldenable = false
 vim.o.foldlevel = 2
 vim.cmd('set indentkeys-=<:>')
-
-vim.cmd('set nu') -- set line numbers
-vim.cmd('set laststatus=2')
-
-vim.cmd('set signcolumn=yes') -- Add signcolumn
-vim.cmd('set splitright') -- Open splits on the right
-vim.cmd('set ignorecase') -- ignore case for search
 
 -- Statusline stuff
 vim.cmd([[
@@ -39,3 +24,19 @@ vim.cmd([[
     set statusline+=%L\ " Total lines
     set showtabline=2
 ]])
+
+-- Misc
+vim.o.pumheight = 25 -- Makes popup menu smaller
+vim.o.fileencoding = "utf-8" -- The encoding written to file
+vim.o.cmdheight = 1 -- Use 2 for more space for displaying messages
+vim.o.mouse = "a" -- Enable your mouse
+vim.o.timeoutlen = 100 -- By default timeoutlen is 1000 ms
+vim.o.undofile = true -- Enable persistent undo
+vim.o.list = true
+vim.o.swapfile = false -- Do not write any swp files
+vim.o.nu = true -- Show line numbers
+vim.o.laststatus = 2 -- Show statusline
+vim.o.ignorecase = true -- Ignore case when searching
+vim.o.signcolumn = "yes" -- Show signs column left of line numbers
+vim.o.splitright = true -- Create a new split on the right
+
