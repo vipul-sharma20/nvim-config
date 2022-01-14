6e00f0f832e5bbbe687c1a665fc5d8b0b2735a59
Vipul Sharma <vipul.sharma20@gmail.com>
2022-01-14 20:49:00 +0530

    add: require module if exist, ignore if doesn't

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
-require("settings")
-require("mappings")
-require("plugins")
-require("autogroups")
-require("dev")
+function loadrequire(module)
+    local function requiref(module)
+        require(module)
+    end
+    res = pcall(requiref,module)
+    if not(res) then
+        print("Module doesn't exist: "..module)
+    end
+end
+
+loadrequire("settings")
+loadrequire("mappings")
+loadrequire("plugins")
+loadrequire("autogroups")
+loadrequire("dev")
