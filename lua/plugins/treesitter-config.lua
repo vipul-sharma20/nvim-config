d181a69d189ed8b6206ac1d94fe03fe1dca0e1f9
Vipul Sharma <vipul.sharma20@gmail.com>
2022-01-15 17:05:50 +0530

    update: treesitter list of plugins to setup always

--- lua/plugins/treesitter-config.lua
+++ lua/plugins/treesitter-config.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "maintained",
+  -- ensure_installed = "maintained",
+  ensure_installed = {"lua", "python", "go", "javascript"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
