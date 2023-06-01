61a8d2e6169bec228c3e2e0caee83a49e8f0fabb
Vipul Sharma <vipul.sharma20@gmail.com>
2023-06-01 18:13:11 +0530

    add: colors for header and visual selection in vimwiki

--- lua/plugins/vimwiki-config.lua
+++ lua/plugins/vimwiki-config.lua
@@ -14,4 +14,15 @@ let g:vimwiki_listsyms = '✗○◐●✓'
 " Open wiki on startup
 " autocmd VimEnter * VimwikiDiaryIndex"
 au BufNewFile ~/tools/noto/diary/*.wiki :silent 0r !~/tools/noto/vimwiki_diary_template '%'
+
+" Kanagawa colors for vimwiki headers
+hi VimwikiHeader1 guifg=#FF9E3B " roninYellow
+hi VimwikiHeader2 guifg=#DCA561 " autumnYellow
+hi VimwikiHeader3 guifg=#957FB8 " oniViolet
+hi VimwikiHeader4 guifg=#938AA9 " springViolet
+hi VimwikiHeader5 guifg=#9CABCA " sprintViolet2
+hi VimwikiHeader6 guifg=#FFFF00
 ]])
+
+-- Change colors for vimwiki visual selction
+vim.api.nvim_set_keymap('v', '<Leader>wc', '<Cmd>VimwikiColorizeNormal<CR>', {noremap = true, silent = true})
